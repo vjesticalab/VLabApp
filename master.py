35a9a1eab23fac79dd276ff5ee5683b91cafcdae
jdorier <33453731+jdorier@users.noreply.github.com>
2023-05-25 13:28:37 +0200

    use QScrollArea inside QTabWidget

--- master.py
+++ master.py
@@ -2,8 +2,8 @@ import sys
 import os
 from functools import partial
 from PyQt5.QtCore import Qt, pyqtSignal, QSize
-from PyQt5.QtGui import QPixmap, QIcon, QFont
-from PyQt5.QtWidgets import QApplication, QSpacerItem, QSizePolicy, QCheckBox, QFrame, QLabel, QPushButton, QVBoxLayout, QWidget, QTabWidget, QFormLayout, QLineEdit, QFileDialog
+from PyQt5.QtGui import QPixmap, QIcon, QFont, QPalette
+from PyQt5.QtWidgets import QApplication, QSpacerItem, QSizePolicy, QCheckBox, QFrame, QLabel, QPushButton, QVBoxLayout, QWidget, QTabWidget, QFormLayout, QLineEdit, QFileDialog, QScrollArea, QFrame
 
 from modules.image_registration_module.registration import registration
 from modules.image_registration_module.alignment import alignment
@@ -43,7 +43,12 @@ class Page(QWidget):
         super().__init__(parent)
         self.container = QWidget()
         lay = QVBoxLayout(self)
-        lay.addWidget(self.container)
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        scroll.setWidget(self.container)
+        scroll.setBackgroundRole(QPalette.Base)
+        scroll.setFrameShape(QFrame.NoFrame)
+        lay.addWidget(scroll)
 
 
 class Home(Page):
@@ -226,98 +231,8 @@ class Segmentation(Page):
     def __init__(self):
         super().__init__()
         self.window = QVBoxLayout(self.container)
-        self.title = QLabel('<b>Image Segmentation</b>', self)
-        self.title.setFont(QFont('Arial', 18))
-        self.title.setAlignment(Qt.AlignCenter)
-        self.window.addWidget(self.title)
-        self.line = QFrame()
-        self.line.setFrameShape(QFrame.HLine)
-        self.line.setMinimumWidth(400)
-        self.window.addWidget(self.line)
-        
-        self.buttonA1 = QPushButton("Individual image")
-        self.buttonA1.setMinimumWidth(200)
-        self.buttonA1.clicked.connect(partial(self.segmentation, 'singleFile'))
-        self.window.addWidget(self.buttonA1, alignment=Qt.AlignCenter)
-
-        self.buttonA2 = QPushButton("Folder of images")
-        self.buttonA2.setMinimumWidth(200)
-        self.buttonA2.clicked.connect(partial(self.segmentation, 'singleFolder'))
-        self.window.addWidget(self.buttonA2, alignment=Qt.AlignCenter)
-
-        self.buttonA3 = QPushButton("Collection of folders")
-        self.buttonA3.setMinimumWidth(200)
-        self.buttonA3.clicked.connect(partial(self.segmentation, 'multiFolder'))
-        self.window.addWidget(self.buttonA3, alignment=Qt.AlignCenter)
-
+        self.window.addWidget(segmentation.Segmentation())
         self.window.addStretch()
-
-        """
-        self.selection_type = ''
-        
-        # Upload
-        self.window.addRow(QLabel("Select individual image"))
-        self.selected_file = QLineEdit()
-        self.browse_button = QPushButton("Browse", self)
-        self.browse_button.clicked.connect(self.browse_file)
-        self.window.addRow(self.selected_file, self.browse_button)
-
-        self.window.addRow(QLabel("Folder of images"))
-        self.selected_folder1 = QLineEdit()
-        self.browse_button1 = QPushButton("Browse", self)
-        self.browse_button1.clicked.connect(self.browse_folder1)
-        self.window.addRow(self.selected_folder1, self.browse_button1)
-        
-        self.window.addRow(QLabel("Collection of folders"))
-        self.displayA = QLabel("Insert here a list of paths to folders. \nEach folder MUST be written in a new line. \nSpace or slash characters at the end of the folder will confuse the script!")
-        self.displayA.setFont(QFont('Arial', 10))
-        self.window.addRow(self.displayA)
-        self.displayB = QLabel("<i>Example: /Users/admin/Desktop/20220216_P0001_E0008_U002</i>")
-        self.window.addRow(self.displayB)
-        self.folders_list = QTextEdit()
-        self.window.addRow(self.folders_list)
-        
-        # Tracking graphs
-        self.graph = QLabel("<i>Note that the tracking graphs are created by default - check the box to skip this step</i>")
-        self.no_graph_generation = QCheckBox("Do NOT create the tracking graphs")
-
-        # Define path where to save
-        self.check_resfolder = QCheckBox("Save results into the save directory (new folder)")
-        self.window.addRow(self.check_resfolder)
-        self.window.addRow(QLabel("OR select another folder where to save results"))
-        self.results_folder = QLineEdit()
-        self.browse_results = QPushButton("Browse", self)
-        self.browse_results.clicked.connect(self.browse_folder_results)
-        self.window.addRow(self.results_folder, self.browse_results)
-        self.line2 = QFrame()
-        self.line2.setFrameShape(QFrame.HLine)
-        self.window.addRow(self.line2)
-
-        # Submit
-        self.submit_button = QPushButton("Submit", self)
-        self.submit_button.clicked.connect(partial(self.segmentation, self.selection_type))
-        self.window.addRow(self.submit_button)
-        self.submission_num_failed = 0
-        self.label_error = None
-        
-    def browse_file(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Select File")
-        self.selected_file.setText(file_path)
-        self.selection_type = 'singleFile'
-
-    def browse_folder1(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
-        self.selected_folder1.setText(folder_path)
-        self.selection_type = 'singleFolder'
-    
-    def browse_folder_results(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
-        self.results_folder.setText(folder_path)"""
-            
-        
-    def segmentation(self, param):
-        window = segmentation.Segmentation(param, parent=self)
-        window.show()     
 
 
 class Tracking(Page):
@@ -393,6 +308,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = MainWindow()
     w.show()
-    w.setFixedWidth(500)
-    w.setFixedHeight(720)
+    #w.setFixedWidth(500)
+    #w.setFixedHeight(720)
     sys.exit(app.exec_())
--- modules/segmentation_module/segmentation/segmentation.py
+++ modules/segmentation_module/segmentation/segmentation.py
@@ -4,10 +4,9 @@ from PyQt5.QtCore import Qt
 from modules.segmentation_module.segmentation import segmentation_functions as f
 from modules.segmentation_module.graphGenerator import graph_functions
 
-class SingleFile(QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.parent = parent
+class Segmentation(QWidget):
+    def __init__(self):
+        super().__init__()
         self.setWindowTitle("Single image segmentation")
         self.setFixedWidth(300)
         self.setFixedHeight(200)
@@ -25,12 +24,6 @@ class SingleFile(QWidget):
         self.submit_button = QPushButton("Submit", self)
         self.submit_button.clicked.connect(self.process_input)
         
-        self.quit_button = QPushButton("Quit", self)
-        self.quit_button.clicked.connect(self.parent.close)
-        self.quit_button.setStyleSheet("background: darkred;")
-        self.line = QFrame()
-        self.line.setFrameShape(QFrame.HLine)
-
         # Layout
         layout = QVBoxLayout()
         layout.addWidget(self.display1)
@@ -39,8 +32,6 @@ class SingleFile(QWidget):
         layout.addWidget(self.display2)
         layout.addWidget(self.no_graph_generation)
         layout.addWidget(self.submit_button)
-        layout.addWidget(self.line)
-        layout.addWidget(self.quit_button)
         self.setLayout(layout)
         self.setFixedWidth(550)
 
@@ -81,181 +72,3 @@ class SingleFile(QWidget):
         else:
             print('\nNo such file found!')
 
-        self.parent.close()
-
-class SingleFolder(QWidget):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-        self.setWindowTitle("Image folder segmentation")
-        self.setFixedWidth(300)
-        
-        self.display = QLabel("<b>Select the folder to process</b>")
-        self.display.setTextFormat(Qt.RichText)
-        self.selected_folder = QLineEdit()
-        
-        self.browse_button = QPushButton("Browse", self)
-        self.browse_button.clicked.connect(self.browse_folder)
-
-        self.display1 = QLabel("<i>Note that the tracking graphs are created by default - check the box to skip this step</i>")
-        self.no_graph_generation = QCheckBox("Do NOT create the tracking graphs")
-        
-        self.submit_button = QPushButton("Submit", self)
-        self.submit_button.clicked.connect(self.process_input)
-
-        self.display2 = QLabel("<i>This step can require also several minutes</i>")
-        
-        self.quit_button = QPushButton("Quit", self)
-        self.quit_button.clicked.connect(self.parent.close)
-        self.quit_button.setStyleSheet("background: darkred;")
-        self.line = QFrame()
-        self.line.setFrameShape(QFrame.HLine)
-
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.display)
-        layout.addWidget(self.selected_folder)
-        layout.addWidget(self.browse_button)
-        layout.addWidget(self.display1)
-        layout.addWidget(self.no_graph_generation)
-        layout.addWidget(self.submit_button) 
-        layout.addWidget(self.display2) 
-        layout.addWidget(self.line)
-        layout.addWidget(self.quit_button)
-        self.setLayout(layout)
-        self.setFixedWidth(550)
-
-    def browse_folder(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
-        self.selected_folder.setText(folder_path)
-
-    def process_input(self):
-        skip_graph_generation = self.no_graph_generation.isChecked()
-        path = self.selected_folder.text() + '/'
-        print('\nSelected folder is:', path)
-
-        if os.path.isdir(path):
-            print('\nFolder found. Generating masks...')
-            try:
-                f.main(path)
-            except Exception as e:
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Critical)
-                msg.setText("Error - segmentation_functions.py failed")
-                msg.setInformativeText(str(e))
-                msg.setWindowTitle("Error")
-                msg.exec_()
-                print('\n')
-                raise(e)
-            if not skip_graph_generation:
-                try:
-                    graph_functions.main(path)
-                except Exception as e:
-                    msg = QMessageBox()
-                    msg.setIcon(QMessageBox.Critical)
-                    msg.setText("Error - graph_functions.py failed")
-                    msg.setInformativeText(str(e))
-                    msg.setWindowTitle("Error")
-                    msg.exec_()
-                    print('\n')
-                    raise(e)
-        else:
-            print('\nNo such folder found!')
-
-        self.parent.close()
-
-class MultiFolder(QWidget):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-        self.setWindowTitle("Multiple folders segmentation")
-        
-        self.display1 = QLabel("<b>Select the folders to process</b>")
-        self.display1.setTextFormat(Qt.RichText)
-
-        self.display1a = QLabel("Insert here a list of paths to folders. \nEach folder MUST be written in a new line. \nSpace or slash characters at the end of the folder will confuse the script!")
-        self.display1b = QLabel("<i>Example: /Users/admin/Desktop/20220216_P0001_E0008_U002</i>")
-        self.display1b.setTextFormat(Qt.RichText)
-        self.folders_list = QTextEdit()
-        
-        self.display2 = QLabel("<i>Note that the tracking graphs are created by default - check the box to skip this step</i>")
-        self.no_graph_generation = QCheckBox("Do NOT create the tracking graphs")
-        
-        self.submit_button = QPushButton("Submit", self)
-        self.submit_button.clicked.connect(self.process_input)
-        self.display3 = QLabel("<i>This step can require also several minutes</i>")
-
-        self.quit_button = QPushButton("Quit", self)
-        self.quit_button.clicked.connect(self.parent.close)
-        self.quit_button.setStyleSheet("background: darkred;")
-        self.line = QFrame()
-        self.line.setFrameShape(QFrame.HLine)
-
-        # Layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.display1)
-        layout.addWidget(self.display1a)
-        layout.addWidget(self.display1b)
-        layout.addWidget(self.folders_list)
-        layout.addWidget(self.display2)
-        layout.addWidget(self.no_graph_generation)
-        layout.addWidget(self.submit_button)
-        layout.addWidget(self.display3) 
-        layout.addWidget(self.line)
-        layout.addWidget(self.quit_button)
-        self.setLayout(layout)
-        self.setFixedWidth(550)
-
-    def process_input(self):
-        skip_graph_generation = self.no_graph_generation.isChecked()
-        folder_list_text = self.folders_list.toPlainText()
-        folder_list = folder_list_text.split('\n')
-        print('\nYou selected ', len(folder_list), 'folders. Selected folders are:')
-        for folder in folder_list:
-            print('\n\t', folder)
-
-        for folder in folder_list:
-            if os.path.isdir(folder):
-                print('\nFolder ', folder, ' found. Generating masks...')
-                path = folder + '/'
-                try:
-                    f.main(path)
-                except Exception as e:
-                    msg = QMessageBox()
-                    msg.setIcon(QMessageBox.Critical)
-                    msg.setText("Error - segmentation_functions.py failed")
-                    msg.setInformativeText(str(e))
-                    msg.setWindowTitle("Error")
-                    msg.exec_()
-                    print('\n')
-                    raise(e)
-                if not skip_graph_generation:
-                    try:
-                        graph_functions.main(path)
-                    except Exception as e:
-                        msg = QMessageBox()
-                        msg.setIcon(QMessageBox.Critical)
-                        msg.setText("Error - graph_functions.py failed")
-                        msg.setInformativeText(str(e))
-                        msg.setWindowTitle("Error")
-                        msg.exec_()
-                        print('\n')
-                        raise(e)
-            else:
-                print('\nUnable to locate folder ', folder, '\nMoving onto next folder...')
-
-        print('\nAll folders have been registered!')
-        self.parent.close()
-
-class Segmentation(QMainWindow):
-    def __init__(self, param, parent=None):
-        super(Segmentation, self).__init__(parent)
-        if param == 'singleFile':
-            form = SingleFile(self)
-            self.setCentralWidget(form)
-        elif param == 'singleFolder':
-            form = SingleFolder(self)
-            self.setCentralWidget(form)
-        elif param == 'multiFolder':
-            form = MultiFolder(self)
-            self.setCentralWidget(form)
