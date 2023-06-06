e03ff307711dfa29cbc2a529d748426a86662cf5
jdorier <33453731+jdorier@users.noreply.github.com>
2023-06-06 09:30:56 +0200

    1) add package version to log. 2) request TYX instead of CYX input image axes. 3) add axes metadata to output file. 4) add file saved notification with QMessageBox when show results in napari.

--- modules/segmentation_module/segmentation/segmentation_functions.py
+++ modules/segmentation_module/segmentation/segmentation_functions.py
@@ -4,13 +4,14 @@ import numpy as np
 import napari
 import tifffile
 from cellpose import models
+from cellpose import version_str as cellpose_version
 from general import general_functions as gf
 
 
 def main(image_path, model_path, output_path, display_results=True, use_gpu=False):
     """
     Load image from `image_path`, segment with cellpose and save the resulting masks
-    into `output_path` directory using image basename (with .tif extension).
+    into `output_path` directory using filename <image basename>_masks.tif.
 
     Parameters
     ----------
@@ -45,19 +46,22 @@ def main(image_path, model_path, output_path, display_results=True, use_gpu=Fals
     logfile_handler.setLevel(logging.INFO)
     logger.addHandler(logfile_handler)
 
-    logger.info("image_path: %s", image_path)
-    logger.info("model_path: %s", model_path)
-    logger.info("output_path: %s", output_path)
-    logger.info("use_gpu: %s", use_gpu)
-    logger.info("display_results: %s", display_results)
+    ##cellpose_version already contains platform, python version and torch version
+    logger.info("System info: %s\nnumpy version: %s\nnapari version: %s",cellpose_version,np.__version__,napari.__version__)
+    logger.info("image: %s", image_path)
+    logger.info("cellpose model: %s", model_path)
+    logger.info("output: %s", output_path)
+    logger.debug("use_gpu: %s", use_gpu)
+    logger.debug("display_results: %s", display_results)
 
     # load image
     logger.debug("loading %s", image_path)
     input_image, axes = gf.open_suitable_files(image_path)
 
     # TODO: deal with Z axis (z-stack projection)
-    if ''.join(list(axes.keys())).upper() != "CYX":
-        raise TypeError(f'Input image is not CYX.\n({image_path})')
+    tmpaxes=''.join(list(axes.keys())).upper()
+    if tmpaxes != "TYX":
+        raise TypeError(f'Input image is ({tmpaxes}) (should be TYX).\n({image_path})')
 
     if display_results:
         # show image in napari
@@ -67,7 +71,6 @@ def main(image_path, model_path, output_path, display_results=True, use_gpu=Fals
     # cellpose: create model
     logger.debug("loading cellpose model %s", model_path)
     model = models.CellposeModel(gpu=use_gpu, pretrained_model=model_path)
-    logger.info("model diameter:%s", model.diam_labels)
 
     if display_results:
         # TODO: find a way to use logging package instead?
@@ -76,6 +79,7 @@ def main(image_path, model_path, output_path, display_results=True, use_gpu=Fals
         # only import PyQt5 if needed (no need for PyQt5 dependency if display_results is False.
         from PyQt5.QtGui import QCursor
         from PyQt5.QtCore import Qt
+        from PyQt5.QtWidgets import QMessageBox
         # set cursor to BusyCursor
         napari.qt.get_app().setOverrideCursor(QCursor(Qt.BusyCursor))
         napari.qt.get_app().processEvents()
@@ -84,6 +88,7 @@ def main(image_path, model_path, output_path, display_results=True, use_gpu=Fals
         pbr = napari.utils.progress(total=input_image.shape[0])
 
     # cellpose segmentation
+    logger.info("Cellpose segmentation (model diameter=%s)", model.diam_labels)
     masks = np.zeros(input_image.shape, dtype='uint16')
     for i in range(input_image.shape[0]):
         if display_results:
@@ -110,11 +115,17 @@ def main(image_path, model_path, output_path, display_results=True, use_gpu=Fals
         # do not allow edition
         layer_masks.editable = False
 
+    # TODO: adapt metadata to more generic input files (other axes)
     # save masks
     output_file = os.path.join(output_path, os.path.splitext(
-        os.path.basename(image_path))[0]+".tif")
-    logger.info("saving segmentation masks to %s", output_file)
-    tifffile.imwrite(output_file, masks, imagej=True, compression='zlib')
+        os.path.basename(image_path))[0]+"_masks.tif")
+    logger.info("Saving segmentation masks to %s", output_file)
+    tifffile.imwrite(output_file, masks, metadata={'axes': 'TYX'},imagej=True, compression='zlib')
+
+    if display_results:
+        QMessageBox.information(viewer_images.window._qt_window,
+                                'File saved',
+                                'Masks saved to\n' + output_file)
 
     # stop using logfile
     logger.removeHandler(logfile_handler)
