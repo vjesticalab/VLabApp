vjesticalab/VLabApp

commit ef5dd326ad4ccd563df386358848aa5f9b9b4d72

minor formatting changes

modules/graph_filtering_module/graph_filtering_functions.py
@@ -3,7 +3,6 @@ import logging
 from platform import python_version, platform
 import numpy as np
 import napari
-import tifffile
 import igraph as ig
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QGridLayout, QWidget, QPushButton, QLabel, QSpinBox, QScrollArea, QGroupBox, QCheckBox, QMessageBox, QSizePolicy
 from PyQt5.QtCore import Qt, QPointF
@@ -433,7 +432,7 @@ class CellTracksFiltering:
                 np.unique(self.mask[:, :, :border_width]),
                 np.unique(self.mask[:, :, -border_width:])]))
         border_mask_ids = border_mask_ids[border_mask_ids > 0]
-        self.selected_cell_track_ids = [i for i in self.selected_cell_track_ids if np.isin(self.cell_tracks[i]['mask_ids'], border_mask_ids).any() == False]
+        self.selected_cell_track_ids = [i for i in self.selected_cell_track_ids if np.isin(self.cell_tracks[i]['mask_ids'], border_mask_ids).any() is False]
 
     def filter_all_cells_area(self, min_area, max_area):
         """
@@ -539,7 +538,7 @@ class CellTracksFiltering:
             indices of topologies for list self.graph_topologies.
         """
 
-        self.logger.info("filtering topology (topologies: %s)",  ", ".join([str(i) for i in selected_topologies]))
+        self.logger.info("filtering topology (topologies: %s)", ", ".join([str(i) for i in selected_topologies]))
         self.selected_cell_track_ids = [i for i in self.selected_cell_track_ids if np.isin(self.cell_tracks[i]['graph_topology'], selected_topologies).any()]
 
     def reset_filters(self):
@@ -651,20 +650,20 @@ class CellTracksFiltering:
         self.logger.info("Saving segmentation mask to %s", output_file)
         selected_mask = self.get_mask(relabel_mask_ids)
         selected_mask = selected_mask[:, np.newaxis, :, :]
-        ome_metadata=OmeTiffWriter.build_ome(data_shapes=[selected_mask.shape],
-                                             data_types=[selected_mask.dtype],
-                                             dimension_order=["TCYX"],
-                                             channel_names=[self.mask_channel_names],
-                                             physical_pixel_sizes=[PhysicalPixelSizes(X=self.mask_physical_pixel_sizes[0], Y=self.mask_physical_pixel_sizes[1], Z=self.mask_physical_pixel_sizes[2])])
-        ome_metadata.structured_annotations.append(CommentAnnotation(value=buffered_handler.get_messages(),namespace="VLabApp"))
+        ome_metadata = OmeTiffWriter.build_ome(data_shapes=[selected_mask.shape],
+                                               data_types=[selected_mask.dtype],
+                                               dimension_order=["TCYX"],
+                                               channel_names=[self.mask_channel_names],
+                                               physical_pixel_sizes=[PhysicalPixelSizes(X=self.mask_physical_pixel_sizes[0], Y=self.mask_physical_pixel_sizes[1], Z=self.mask_physical_pixel_sizes[2])])
+        ome_metadata.structured_annotations.append(CommentAnnotation(value=buffered_handler.get_messages(), namespace="VLabApp"))
         for x in self.metadata:
-            ome_metadata.structured_annotations.append(CommentAnnotation(value=x,namespace="VLabApp"))
+            ome_metadata.structured_annotations.append(CommentAnnotation(value=x, namespace="VLabApp"))
         OmeTiffWriter.save(selected_mask, output_file, ome_xml=ome_metadata)
 
         output_file = os.path.join(output_path, output_basename+".graphmlz")
         self.logger.info("Saving cell tracking graph to %s", output_file)
         g = self.get_graph(relabel_mask_ids)
-        #add metadata
+        # add metadata
         g['VLabApp:Annotation:1'] = buffered_handler.get_messages()
         for i, x in enumerate(self.metadata):
             g['VLabApp:Annotation:'+str(i+2)] = x
@@ -1039,7 +1038,7 @@ class GraphFilteringWidget(QWidget):
     def on_viewer_images_close(self):
         if self.mask_modified:
             if self.mask_need_filtering:
-                save = QMessageBox.question(self, 'Save changes',  "Filter and save changes before closing?", QMessageBox.Yes | QMessageBox.No)
+                save = QMessageBox.question(self, 'Save changes', "Filter and save changes before closing?", QMessageBox.Yes | QMessageBox.No)
             else:
                 save = QMessageBox.question(self, 'Save changes', "Save changes before closing?", QMessageBox.Yes | QMessageBox.No)
             if save == QMessageBox.Yes:
@@ -1148,7 +1147,7 @@ def main(image_path, mask_path, graph_path, output_path, output_basename, filter
         try:
             image = gf.Image(image_path)
             image.imread()
-        except:
+        except Exception:
             logger.exception('Error loading image %s', image_path)
             # stop using logfile
             logger.removeHandler(logfile_handler)
@@ -1160,17 +1159,17 @@ def main(image_path, mask_path, graph_path, output_path, output_basename, filter
     try:
         mask = gf.Image(mask_path)
         mask.imread()
-    except:
+    except Exception:
         logger.exception('Error loading mask %s', mask_path)
         # stop using logfile
         logger.removeHandler(logfile_handler)
         logger.removeHandler(buffered_handler)
         raise
 
-    #load mask metadata
+    # load mask metadata
     mask_metadata = []
     if mask.ome_metadata:
-        for i,x in enumerate(mask.ome_metadata.structured_annotations):
+        for i, x in enumerate(mask.ome_metadata.structured_annotations):
             if isinstance(x, CommentAnnotation) and x.namespace == "VLabApp":
                 if len(mask_metadata) == 0:
                     mask_metadata.append("Metadata for "+mask.path+":\n"+x.value)
@@ -1179,9 +1178,9 @@ def main(image_path, mask_path, graph_path, output_path, output_basename, filter
 
     # Load graph
     logger.debug("loading %s", graph_path)
-    graph = gf.load_cell_tracking_graph(graph_path,mask.image.dtype)
+    graph = gf.load_cell_tracking_graph(graph_path, mask.image.dtype)
 
-    #graph metadata
+    # graph metadata
     graph_metadata = []
     for a in graph.attributes():
         if a.startswith('VLabApp:Annotation'):
@@ -1203,12 +1202,12 @@ def main(image_path, mask_path, graph_path, output_path, output_basename, filter
         layer.editable = False
         # In the current version of napari (v0.4.17), editable is set to True whenever we change the axis value by clicking on the corresponding slider.
         # This is a quick and dirty hack to force the layer to stay non-editable.
-        layer.events.editable.connect(lambda e: setattr(e.source,'editable',False))
+        layer.events.editable.connect(lambda e: setattr(e.source, 'editable', False))
         selected_mask_layer = viewer_images.add_labels(mask.get_TYXarray(), name="Selected cell mask")
         selected_mask_layer.editable = False
         # In the current version of napari (v0.4.17), editable is set to True whenever we change the axis value by clicking on the corresponding slider.
         # This is a quick and dirty hack to force the layer to stay non-editable.
-        selected_mask_layer.events.editable.connect(lambda e: setattr(e.source,'editable',False))
+        selected_mask_layer.events.editable.connect(lambda e: setattr(e.source, 'editable', False))
 
         # add GraphFilteringWidget to napari
         scroll_area = QScrollArea()

general/general_functions.py
@@ -317,17 +317,13 @@ class FileTableWidget2(QWidget):
         self.header_2 = header_2
         self.filter_name = QLineEdit(filenames_filter, placeholderText='e.g.: _BF')
         self.filter_name.setToolTip('Accept only filenames containing this text. Filtering is done only when populating the table.')
-        self.filter_name.textChanged.connect(self.filter_name_changed)
         self.filter_name_exclude = QLineEdit(filenames_exclude_filter, placeholderText='e.g.: _WL508')
         self.filter_name_exclude.setToolTip('Accept only filenames NOT containing this text. Filtering is done only when populating the table.')
-        self.filter_name_exclude.textChanged.connect(self.filter_name_exclude_changed)
 
         self.suffix_1 = QLineEdit(filenames_suffix_1, placeholderText='e.g.: _vTG.ome.tif')
         self.suffix_1.setToolTip('Accept only filenames ending with this text.')
-        self.suffix_1.textChanged.connect(self.suffix_1_changed)
         self.suffix_2 = QLineEdit(filenames_suffix_2, placeholderText='e.g.: _vTG.graphmlz')
         self.suffix_2.setToolTip('Accept only filenames ending with this text')
-        self.suffix_2.textChanged.connect(self.suffix_2_changed)
         self.file_table = DropFilesTableWidget2(self.filter_files, header_1=header_1, header_2=header_2)
         self.file_table.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.file_table.setSelectionBehavior(QAbstractItemView.SelectRows)
@@ -376,18 +372,6 @@ class FileTableWidget2(QWidget):
 
     def file_table_rows_removed(self):
         self.file_table_changed.emit()
-
-    def filter_name_changed(self):
-        self.file_table.filenames_filter = self.filter_name.text()
-
-    def filter_name_exclude_changed(self):
-        self.file_table.filenames_exclude_filter = self.filter_name_exclude.text()
-
-    def suffix_1_changed(self):
-        self.file_table.filenames_suffix_1 = self.suffix_1.text()
-
-    def suffix_2_changed(self):
-        self.file_table.filenames_suffix_2 = self.suffix_2.text()
 
     def add_file(self):
         type_list = ['*'+self.suffix_1.text(), '*'+self.suffix_2.text()]
@@ -472,10 +456,8 @@ class ImageMatrixTableWidget2(QWidget):
 
         self.filter_name = QLineEdit(filenames_filter, placeholderText='e.g.: _BF')
         self.filter_name.setToolTip('Accept only filenames containing this text. Filtering is done only when populating the table.')
-        self.filter_name.textChanged.connect(self.filter_name_changed)
         self.filter_name_exclude = QLineEdit(filenames_exclude_filter, placeholderText='e.g.: _WL508')
         self.filter_name_exclude.setToolTip('Accept only filenames NOT containing this text. Filtering is done only when populating the table.')
-        self.filter_name_exclude.textChanged.connect(self.filter_name_exclude_changed)
 
         self.file_table = DropFilesTableWidget2(self.filter_files, header_1='Image', header_2='Matrix')
         self.file_table.setSelectionMode(QAbstractItemView.ExtendedSelection)
@@ -509,12 +491,6 @@ class ImageMatrixTableWidget2(QWidget):
         layout.addWidget(help_label)
         layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(layout)
-
-    def filter_name_changed(self):
-        self.file_table.filenames_filter = self.filter_name.text()
-
-    def filter_name_exclude_changed(self):
-        self.file_table.filenames_exclude_filter = self.filter_name_exclude.text()
 
     def add_file(self):
         type_list = ['*'+x for x in imagetypes]
@@ -671,13 +647,10 @@ class FileListWidget(QWidget):
             filetypes = []
         self.filter_name = QLineEdit(filenames_filter, placeholderText='e.g.: _BF')
         self.filter_name.setToolTip('Accept only filenames containing this text. Filtering is done only when populating the list.')
-        self.filter_name.textChanged.connect(self.filter_name_changed)
         self.filter_name_exclude = QLineEdit(filenames_exclude_filter, placeholderText='e.g.: _WL508')
         self.filter_name_exclude.setToolTip('Accept only filenames NOT containing this text. Filtering is done only when populating the list.')
-        self.filter_name_exclude.textChanged.connect(self.filter_name_exclude_changed)
         self.filetypes = QLineEdit(' '.join(filetypes), placeholderText='e.g.: .nd2 .tif .tiff .ome.tif .ome.tiff')
         self.filetypes.setToolTip('Space separated list of accepted file extensions. Filtering is done only when populating the list.')
-        self.filetypes.textChanged.connect(self.filetypes_changed)
         self.file_list = DropFilesListWidget(self.filter_files)
         self.file_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.file_list.model().rowsInserted.connect(self.file_list_rows_inserted)
@@ -721,15 +694,6 @@ class FileListWidget(QWidget):
 
     def file_list_rows_removed(self):
         self.file_list_changed.emit()
-
-    def filter_name_changed(self):
-        self.file_list.filenames_filter = self.filter_name.text()
-
-    def filter_name_exclude_changed(self):
-        self.file_list.filenames_exclude_filter = self.filter_name_exclude.text()
-
-    def filetypes_changed(self):
-        self.file_list.filetypes = self.filetypes.text().split()
 
     def add_file(self):
         type_list = ['*'+x for x in self.filetypes.text().split()]
@@ -833,10 +797,8 @@ class FolderListWidget(QWidget):
 
         self.filter_name = QLineEdit(foldernames_filter, placeholderText='e.g.: _BF')
         self.filter_name.setToolTip('Accept only folder names containing this text. Filtering is done only when populating the list.')
-        self.filter_name.textChanged.connect(self.filter_name_changed)
         self.filter_name_exclude = QLineEdit(foldernames_exclude_filter, placeholderText='e.g.: _WL508')
         self.filter_name_exclude.setToolTip('Accept only folder names NOT containing this text. Filtering is done only when populating the list.')
-        self.filter_name_exclude.textChanged.connect(self.filter_name_exclude_changed)
         self.folder_list = DropFoldersListWidget(self.filter_folders)
         self.folder_list.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.folder_list.model().rowsInserted.connect(self.folder_list_rows_inserted)
@@ -874,12 +836,6 @@ class FolderListWidget(QWidget):
 
     def folder_list_rows_removed(self):
         self.folder_list_changed.emit()
-
-    def filter_name_changed(self):
-        self.folder_list.foldernames_filter = self.filter_name.text()
-
-    def filter_name_exclude_changed(self):
-        self.folder_list.foldernames_exclude_filter = self.filter_name_exclude.text()
 
     def add_folder(self):
         folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
@@ -1122,7 +1078,7 @@ class Image:
             raise TypeError('Image format not supported. Please upload a tiff, ome-tiff or nd2 image file.')
 
         self.shape = []
-        self.sizes = dict()
+        self.sizes = {}
         for a in self._axes:
             if a in axes_order:
                 self.shape.append(shape[axes_order.index(a)])
@@ -1283,7 +1239,7 @@ class Image:
                         projected_image[f, t, c, 0, :, :] = np.min(self.image[f, t, c, z_values, :, :], axis=0)
                     elif projection_type == 'std':
                         projected_image[f, t, c, 0, :, :] = np.std(self.image[f, t, c, z_values, :, :], axis=0, ddof=1)
-                    elif projection_type == 'avg' or projection_type == 'mean':
+                    elif projection_type in ['avg', 'mean']:
                         projected_image[f, t, c, 0, :, :] = np.mean(self.image[f, t, c, z_values, :, :], axis=0)
                     elif projection_type == 'median':
                         projected_image[f, t, c, 0, :, :] = np.median(self.image[f, t, c, z_values, :, :], axis=0)
@@ -1344,19 +1300,6 @@ def update_transfMat(tmat_int, reference_timepoint_index, range_start_index, ran
     return tmat_updated
 
 
-def error_empty(submission_num, widget, window):
-    """
-    Add an error line in the main application window when missing input values
-    """
-    widget.setFocus()
-    if submission_num == 1:
-        label_error = QLabel('Error : missing value')
-        label_error.setAlignment(Qt.AlignCenter)
-        label_error.setStyleSheet("color: red;")
-        window.addRow(label_error)
-        return label_error
-
-
 def load_cell_tracking_graph(graph_path, mask_dtype):
     graph = ig.Graph().Read_GraphMLz(graph_path)
     # Adjust attibute types
@@ -1411,7 +1354,7 @@ def evaluate_graph_properties(graph):
         # Number of missing mask regions (edges spanning more than 1 frame)
         n_missing = np.sum([e['frame_target'] - e['frame_source'] - 1 for e in g2.es])
         # Number fusion events with stable neighborhood
-        n_fusions = np.sum([1 if v.indegree() > 1 and min([v2['stable_component_size'] for v2 in v.neighbors()]) >= 1 else 0 for v in g2.vs])
+        n_fusions = np.sum([1 if v.indegree() > 1 and min(v2['stable_component_size'] for v2 in v.neighbors()) >= 1 else 0 for v in g2.vs])
         fusions_frames = []
         if n_fusions > 0:
             indegree = g2.vs[0].indegree()
@@ -1420,7 +1363,7 @@ def evaluate_graph_properties(graph):
                     indegree = v.indegree()
                     fusions_frames.append(v['frame'])
         # Number division events with stable neighborhood
-        n_divisions = np.sum([1 if v.outdegree() > 1 and min([v2['stable_component_size'] for v2 in v.neighbors()]) >= 1 else 0 for v in g2.vs])
+        n_divisions = np.sum([1 if v.outdegree() > 1 and min(v2['stable_component_size'] for v2 in v.neighbors()) >= 1 else 0 for v in g2.vs])
         divisions_frames = []
         if n_divisions > 0:
             outdegree = g2.vs[0].outdegree()

master.py
@@ -2,8 +2,8 @@ import os
 import sys
 import logging
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap, QFont, QIcon
-from PyQt5.QtWidgets import QApplication, QFrame, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFormLayout, QLineEdit
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit
 
 from modules.registration_module import registration
 from modules.zprojection_module import zprojection
@@ -73,7 +73,7 @@ class MainWindow(QWidget):
         window.addLayout(layout)
 
         # Setup logging
-        logging.basicConfig(level=logging.INFO, format="%(asctime)s (%(name)s) [%(levelname)s] %(message)s", handlers=[logging.StreamHandler(sys.stdout)],force=True)
+        logging.basicConfig(level=logging.INFO, format="%(asctime)s (%(name)s) [%(levelname)s] %(message)s", handlers=[logging.StreamHandler(sys.stdout)], force=True)
 
         # Add a handler to output messages to self.status_line
         self.qlabel_handler = gf.QLineEditHandler(self.status_line)
@@ -105,5 +105,5 @@ if __name__ == "__main__":
     app.setWindowIcon(QIcon('support_files/Vlab_icon_50x50-01.png'))
     w = MainWindow()
     w.show()
-    w.resize(900,800)
+    w.resize(900, 800)
     sys.exit(app.exec_())

modules/cell_tracking_module/cell_tracking.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import re
 from PyQt5.QtWidgets import QFileDialog, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QGroupBox, QRadioButton, QApplication, QSpinBox, QFormLayout, QLineEdit
 from PyQt5.QtCore import Qt, QRegExp
 from PyQt5.QtGui import QCursor, QRegExpValidator
@@ -20,8 +19,8 @@ class CellTracking(QWidget):
         label_documentation = QLabel()
         label_documentation.setOpenExternalLinks(True)
         label_documentation.setWordWrap(True)
-        label_documentation.setText('For each input segmentation mask, perform cell tracking, save the cell tracking graph and segmentation mask with relabelled cells.<br>'+
-                                    'Input segmentation mask must have X, Y and T axes. The optional input image must have X, Y and T axes and can optionally have C and/or Z axes.<br><br>'+
+        label_documentation.setText('For each input segmentation mask, perform cell tracking, save the cell tracking graph and segmentation mask with relabelled cells.<br>' +
+                                    'Input segmentation mask must have X, Y and T axes. The optional input image must have X, Y and T axes and can optionally have C and/or Z axes.<br><br>' +
                                     'Additional information: <a href="file://'+os.path.join(os.path.dirname(__file__), "doc", "METHODS.html")+'">Methods</a>')
 
         self.mask_list = gf.FileListWidget(filetypes=gf.imagetypes, filenames_filter=self.mask_suffix)
@@ -118,7 +117,7 @@ class CellTracking(QWidget):
         collapsible_widget = gf.CollapsibleWidget('', collapsed_icon="▶ (show)", expanded_icon="▼ (hide)", expanded=False)
         collapsible_widget.content.setLayout(QVBoxLayout())
         collapsible_widget.content.layout().addWidget(label_documentation)
-        collapsible_widget.content.layout().setContentsMargins(0,0,0,0)
+        collapsible_widget.content.layout().setContentsMargins(0, 0, 0, 0)
         layout2.addWidget(collapsible_widget)
         groupbox.setLayout(layout2)
         layout.addWidget(groupbox)
@@ -237,8 +236,8 @@ class CellTracking(QWidget):
         else:
             output_path = self.output_folder.text().rstrip("/")
 
-        self.output_filename_label1.setText(os.path.join(output_path,"<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".ome.tif"))
-        self.output_filename_label2.setText(os.path.join(output_path,"<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".graphmlz"))
+        self.output_filename_label1.setText(os.path.join(output_path, "<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".ome.tif"))
+        self.output_filename_label2.setText(os.path.join(output_path, "<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".graphmlz"))
 
     def get_widgets_state(self):
         widgets_state = {

modules/events_filter_module/events_filter_functions.py
@@ -225,7 +225,7 @@ def save_cropped_events(events_list, n_tp, total_events_mask, marker_image, chcr
 
         # Save
         output_name=os.path.join(crop_output_path, crop_output_basename+'-'+str(n)+'.ome.tif')
-        logger.info("Saving cropped mask %d to %s",n, output_name)
+        logging.getLogger(__name__).info("Saving cropped mask %d to %s",n, output_name)
         ome_metadata=OmeTiffWriter.build_ome(data_shapes=[cropped_mask.shape],
                                              data_types=[cropped_mask.dtype],
                                              dimension_order=["TCYX"],

modules/file_organization_module/file_organization.py
@@ -295,7 +295,7 @@ class FileOrganization(QWidget):
         input_paths = self.folder_list.get_folder_list()
         if len(input_paths) == 0:
             self.logger.error('Input folder missing')
-            return False
+            return
 
         if self.use_input_folder.isChecked():
             output_paths = [os.path.join(os.path.dirname(path), os.path.basename(path.rstrip('/'))) for path in input_paths]
@@ -354,7 +354,7 @@ class FileOrganization(QWidget):
         input_paths = self.folder_list.get_folder_list()
         if len(input_paths) == 0:
             self.logger.error('Input folder missing')
-            return False
+            return
 
         patterns = []
         if self.clean_zprojection.isChecked():

modules/graph_filtering_module/graph_filtering.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import re
 import igraph as ig
 from PyQt5.QtWidgets import QFileDialog, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QGroupBox, QRadioButton, QApplication, QLabel, QFormLayout, QSpinBox, QCheckBox, QSizePolicy, QLineEdit
 from PyQt5.QtCore import Qt, QRegExp
@@ -23,14 +22,14 @@ class GraphFiltering(QWidget):
         label_documentation = QLabel()
         label_documentation.setOpenExternalLinks(True)
         label_documentation.setWordWrap(True)
-        label_documentation.setText('For each pair of input segmentation mask and cell tracking graph, apply the selected filters and save the filtered cell tracking graph and segmentation mask.<br>'+
+        label_documentation.setText('For each pair of input segmentation mask and cell tracking graph, apply the selected filters and save the filtered cell tracking graph and segmentation mask.<br>' +
                                     'Input segmentation mask must have X, Y and T axes. The optional input image must have X, Y and T axes and can optionally have C and/or Z axes.')
         groupbox = QGroupBox("Documentation")
         layout2 = QVBoxLayout()
         collapsible_widget = gf.CollapsibleWidget('', collapsed_icon="▶ (show)", expanded_icon="▼ (hide)", expanded=False)
         collapsible_widget.content.setLayout(QVBoxLayout())
         collapsible_widget.content.layout().addWidget(label_documentation)
-        collapsible_widget.content.layout().setContentsMargins(0,0,0,0)
+        collapsible_widget.content.layout().setContentsMargins(0, 0, 0, 0)
         layout2.addWidget(collapsible_widget)
         groupbox.setLayout(layout2)
         layout.addWidget(groupbox)
@@ -374,8 +373,8 @@ class GraphFiltering(QWidget):
         else:
             output_path = self.output_folder.text().rstrip("/")
 
-        self.output_filename_label1.setText(os.path.join(output_path,"<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".ome.tif"))
-        self.output_filename_label2.setText(os.path.join(output_path,"<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".graphmlz"))
+        self.output_filename_label1.setText(os.path.join(output_path, "<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".ome.tif"))
+        self.output_filename_label2.setText(os.path.join(output_path, "<input basename>" + self.output_suffix + self.output_user_suffix.text() + ".graphmlz"))
 
     def get_widgets_state(self):
         widgets_state = {

commit 0d24dd1dca58cda6e1900f55b9337b93f6fab872

bugfix: filter out cell tracks with at least one cell touching the border (bug introduced in commit ef5dd326ad4ccd563df386358848aa5f9b9b4d72)

modules/graph_filtering_module/graph_filtering_functions.py
@@ -432,7 +432,7 @@ class CellTracksFiltering:
                 np.unique(self.mask[:, :, :border_width]),
                 np.unique(self.mask[:, :, -border_width:])]))
         border_mask_ids = border_mask_ids[border_mask_ids > 0]
-        self.selected_cell_track_ids = [i for i in self.selected_cell_track_ids if np.isin(self.cell_tracks[i]['mask_ids'], border_mask_ids).any() is False]
+        self.selected_cell_track_ids = [i for i in self.selected_cell_track_ids if not np.isin(self.cell_tracks[i]['mask_ids'], border_mask_ids).any()]
 
     def filter_all_cells_area(self, min_area, max_area):
         """