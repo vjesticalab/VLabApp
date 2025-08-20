89cb3e2cfdaccc8f7b7598d747bf8cc88fa758e9
jdorier <33453731+jdorier@users.noreply.github.com>
2025-08-20 15:43:44 +0200

    Cell shapes plugin: add option to choose output units

--- plugins/cell_shapes/__init__.py
+++ plugins/cell_shapes/__init__.py
@@ -5,8 +5,9 @@ A plugin to evaluate shape properties of segmented cells.
 import os
 import logging
 import pandas as pd
+import numpy as np
 from skimage.measure import regionprops_table
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QFormLayout, QLabel, QGroupBox, QRadioButton, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QFormLayout, QLabel, QGroupBox, QRadioButton, QPushButton, QLineEdit, QComboBox
 from PyQt5.QtCore import Qt
 from general import general_functions as gf
 
@@ -64,8 +65,27 @@ class Widget(QWidget):
         groupbox.setLayout(layout2)
         layout.addWidget(groupbox)
 
+        groupbox = QGroupBox("Options")
+        layout2 = QFormLayout()
+        layout2.addRow(QLabel('Output units:'))
+        self.length_unit = QComboBox()
+        self.length_unit.addItem('pixels')
+        self.length_unit.addItem('µm')
+        self.length_unit.setCurrentText('pixels')
+        layout2.addRow('Length:', self.length_unit)
+        self.angle_unit = QComboBox()
+        self.angle_unit.addItem('radian')
+        self.angle_unit.addItem('degree')
+        self.angle_unit.setCurrentText('radian')
+        layout2.addRow('Angle:', self.angle_unit)
+        label = QLabel('<i>Conversion to µm uses the physical pixel sizes stored in the mask metadata. Please verify that these values are correctly set using the "View metadata" module.</i>')
+        label.setWordWrap(True)
+        layout2.addRow(label)
+        groupbox.setLayout(layout2)
+        layout.addWidget(groupbox)
+
         # Submit button
-        self.submit_button = QPushButton("Submit")
+        self.submit_button = QPushButton('Submit')
         self.submit_button.clicked.connect(self.submit)
         layout.addWidget(self.submit_button, alignment=Qt.AlignCenter)
 
@@ -85,7 +105,7 @@ class Widget(QWidget):
                 mask_TYX = mask.get_TYXarray()
 
                 # Evaluate shape properties
-                properties = []
+                properties_frame = []
                 for t in range(mask.sizes['T']):
                     props = regionprops_table(mask_TYX[t, :, :],
                                               properties=('label',
@@ -99,16 +119,48 @@ class Widget(QWidget):
                     df = pd.DataFrame(props)
                     # add a column 'frame' (time frame)
                     df.insert(0, 'frame', t)
-                    # rename columns
-                    df.rename(columns={'label': 'mask_id',
-                                       'centroid-0': 'centroid_y',
-                                       'centroid-1': 'centroid_x'},
-                              inplace=True)
-                    properties.append(df)
+                    properties_frame.append(df)
+
+                properties = pd.concat(properties_frame, ignore_index=True)
+                # rename columns
+                properties.rename(columns={'label': 'mask_id',
+                                           'centroid-0': 'centroid_y[pixels]',
+                                           'centroid-1': 'centroid_x[pixels]',
+                                           'area': 'area[pixels^2]',
+                                           'perimeter': 'perimeter[pixels]',
+                                           'axis_major_length': 'axis_major_length[pixels]',
+                                           'axis_minor_length': 'axis_minor_length[pixels]',
+                                           'orientation': 'orientation[rad]'},
+                                  inplace=True)
+                if self.length_unit.currentText() == 'µm' and mask.physical_pixel_sizes:
+                    # convert lengths to µm
+                    psize = (mask.physical_pixel_sizes[0] + mask.physical_pixel_sizes[1]) / 2.0
+                    logging.getLogger(__name__).info('Converting lengths to %s (Physical pixel sizes: %s µm)', self.length_unit.currentText(), psize)
+                    properties['centroid_y[pixels]'] = properties['centroid_y[pixels]'] * psize
+                    properties['centroid_x[pixels]'] = properties['centroid_x[pixels]'] * psize
+                    properties['perimeter[pixels]'] = properties['perimeter[pixels]'] * psize
+                    properties['axis_major_length[pixels]'] = properties['axis_major_length[pixels]'] * psize
+                    properties['axis_minor_length[pixels]'] = properties['axis_minor_length[pixels]'] * psize
+                    properties['area[pixels^2]'] = properties['area[pixels^2]'] * psize**2
+                    #rename columns
+                    properties.rename(columns={'centroid_y[pixels]': 'centroid_y[um]',
+                                               'centroid_x[pixels]': 'centroid_x[um]',
+                                               'area[pixels^2]': 'area[um^2]',
+                                               'perimeter[pixels]': 'perimeter[um]',
+                                               'axis_major_length[pixels]': 'axis_major_length[um]',
+                                               'axis_minor_length[pixels]': 'axis_minor_length[um]'},
+                                      inplace=True)
+                if self.angle_unit.currentText() == 'degree' and mask.physical_pixel_sizes:
+                    logging.getLogger(__name__).info('Converting angles to %s.', self.angle_unit.currentText())
+                    # convert angles to degree
+                    properties['orientation[rad]'] = properties['orientation[rad]'] * 180 / np.pi
+                    #rename columns
+                    properties.rename(columns={'orientation[rad]': 'orientation[deg]'},
+                                      inplace=True)
 
                 # Save table
                 logging.getLogger(__name__).info('Creating: %s', output_filename)
-                pd.concat(properties, ignore_index=True).to_csv(output_filename, sep='\t', na_rep='nan', index=False)
+                properties.to_csv(output_filename, sep='\t', na_rep='nan', index=False)
 
             except Exception:
                 logging.getLogger(__name__).exception("Processing failed for mask:\n%s\n\nError message:", input_path)
